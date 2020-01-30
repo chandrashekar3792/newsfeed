1ec60d18974e6f9e7a2a1247a123e4a9fc165c5c
chandrashekar3792 <chandrashekar998@gmail.com>
2020-01-30 22:53:56 +0530

    Updates

---
+++ precache-manifest.0062bcb84cdc5c5ebbd127f97dec9e94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb852e6317775a9a3adff69abd840fd",
+    "url": "/newsfeed/index.html"
+  },
+  {
+    "revision": "7368eb3513c0f60e36ce",
+    "url": "/newsfeed/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a7c8241e0d9e713c29cc",
+    "url": "/newsfeed/static/css/main.f234baba.chunk.css"
+  },
+  {
+    "revision": "7368eb3513c0f60e36ce",
+    "url": "/newsfeed/static/js/2.931add6d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/newsfeed/static/js/2.931add6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7c8241e0d9e713c29cc",
+    "url": "/newsfeed/static/js/main.d88825e1.chunk.js"
+  },
+  {
+    "revision": "db8dd3be82499f0a96a9",
+    "url": "/newsfeed/static/js/runtime-main.77992302.js"
+  }
+]);
